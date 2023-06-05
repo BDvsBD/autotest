6d71de2e429411177f0a1b6f0a223f08bb6953f2
eo.shabarkovskiy <eo.shabarkovskiy@tensor.ru>
2023-06-05 14:47:25 +0300

    homevorl_9_2

--- lection 9/task_1.py
+++ lection 9/task_1.py
@@ -1,4 +1,21 @@
-purchase = '6110\n6110\n6110\n6110\n6110\n6110\n6110\n6110\n6110\n6110\n'
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
 
 
-print(map(int, purchase.strip().split('\n')))
+# Здесь пишем код
+with open("test_file/task1_data.txt", "r", encoding='utf-8') as f:
+    text = f.read()
+    text = "".join([word for word in text if not word.isdigit()])
+
+with open("test_file/task1_answer.txt", "w", encoding='utf-8') as f:
+    f.write(text)
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
