1907229969efc12a156b0942dbd5ea6a74075ab7
eo.shabarkovskiy <eo.shabarkovskiy@tensor.ru>
2023-06-14 08:16:34 +0300

    'task_2_new'

--- lection 11/task_2.py
+++ lection 11/task_2.py
@@ -8,14 +8,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
 driver = webdriver.Chrome()
 site = 'https://fix-sso.sbis.ru/auth-online/?ret=fix-online.sbis.ru'
 site_title = 'Вход в личный кабинет'
 action_chains = ActionChains(driver)
 try:
     driver.get(site)
-    sleep(1)
     print('Проверить адрес сайта и заголовок страницы')
     assert driver.current_url == site, 'Неверный адрес сайта'
     assert driver.title == site_title, 'Неверный заголовок сайта'
@@ -27,32 +29,39 @@ try:
     password = driver.find_element(By.CSS_SELECTOR, '[name="Password"]')
     password.send_keys(user_password, Keys.ENTER)
     sleep(5)
-
     print('Навести курсор на сообщения')
-    contacts = driver.find_element(By.CSS_SELECTOR, '.informers-InformersBar-informer-header_container'
-                                                    '[title="Новое сообщение"]')
+    contacts = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[data-qa="informers_informerContent_Messages_icon"]')))
     contacts.click()
-    sleep(1)
     receiver = 'Кассирыч Кар Иванов'
-    search = driver.find_element(By.CSS_SELECTOR, '.controls-StackTemplate-content .controls-Field')
+    search = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.controls-StackTemplate-content .controls-Field')))
     search.send_keys(receiver, Keys.ENTER)
-    search.send_keys(Keys.ENTER)
+    addressee = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.msg-addressee-selector_plain-list [title="Кассирыч Кар"]')))
+    addressee.click()
     message_text = 'Тестовое сообщение'
-    message = driver.find_element(By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph')
+    message = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph')))
     message.send_keys(message_text)
-    send = driver.find_element(By.CSS_SELECTOR, '.controls-StackTemplate__content-area [title="Отправить"]')
+    send = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.controls-Button_clickable[data-qa="msg-send-editor__send-button"]')))
     send.click()
-    received_message = driver.find_element(By.CSS_SELECTOR, '[data-qa="counter_new"]')
+    received_message = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[data-qa="counter_new"]')))
     received_message.click()
-    receiver_name = driver.find_element(By.CSS_SELECTOR, '.msg-dialogs-item__addressee_limited[title="Кассирыч Кар"]')
-    receiver_text = driver.find_element(By.CSS_SELECTOR, '.msg-dialogs-item__message-text')
-    assert receiver_name.text == 'Кассирыч Кар', receiver_text.text == message_text
-    unread_message = driver.find_element(By.CSS_SELECTOR, '.layout-Browser__content .controls-BaseControl')
+    receiver_name = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.msg-dialogs-item__addressee_limited[title="Кассирыч Кар"]')))
+    assert receiver_name.text == 'Кассирыч Кар'
+    unread_message = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.layout-Browser__content .controls-BaseControl')))
     action_chains.context_click(unread_message)
     action_chains.perform()
-    message_del = driver.find_elements(By.CSS_SELECTOR, '.controls-Menu__content-wrapper_width')[5]
+    message_del = WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.controls-Menu__content-wrapper_width')))[5]
     message_del.click()
-    empty = driver.find_element(By.CSS_SELECTOR, '.hint-Template__text_message')
+    empty = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.hint-Template__text_message')))
     assert empty.text == 'Не найдено ни одного сообщения', 'Элемент не отображается'
     print('Все тесты прошли')
 
--- lection 11/task_with_star.py
+++ lection 11/task_with_star.py
@@ -6,3 +6,43 @@
 # Вывести на печать размер скачанного файла в мегабайтах
 # Для сдачи задания пришлите код и запись с экрана прохождения теста
 
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys, ActionChains
+from time import sleep
+import os
+
+driver = webdriver.Chrome()
+site = 'https://sbis.ru/'
+site_title = 'СБИС — экосистема для бизнеса: учет, управление и коммуникации'
+action_chains = ActionChains(driver)
+try:
+    driver.get(site)
+    sleep(1)
+    print('Проверить адрес сайта и заголовок страницы')
+    assert driver.current_url == site, 'Неверный адрес сайта'
+    assert driver.title == site_title, 'Неверный заголовок сайта'
+    footer = driver.find_element(By.CSS_SELECTOR, '.sbisru-Footer')
+    footer.location_once_scrolled_into_view
+    download_link = driver.find_element(By.CSS_SELECTOR, '.sbisru-Footer__link[href="/download?tab=ereport&innerTab=ereport25"]')
+    download_link.click()
+    sbis_plaguin = driver.find_elements(By.CSS_SELECTOR, '.controls-TabButton__right-align.controls-ListView__item ')[1]
+    print(sbis_plaguin.text)
+    sbis_plaguin.click()
+    download_link = driver.find_element(By.CSS_SELECTOR, '[href = "https://update.sbis.ru/Sbis3Plugin/master/win32/sbisplugin-setup-web.exe"] ')
+    download_link.click()
+
+    download_folder = os.path.dirname(os.path.abspath(__file__))
+    sleep(5)
+
+    # находим скачанный файл и выводим его размер в мегабайтах
+    for file in os.listdir(download_folder):
+        if file.endswith('.exe'):
+            file_size = os.path.getsize(os.path.join(download_folder, file))
+            print(f'Размер файла {file} составляет {round(file_size / 1048576, 2)} МБ')
+
+
+finally:
+    driver.quit()
+
